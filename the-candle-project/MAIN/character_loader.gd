fe4a7706561b392803de766fd2c4ace1ccd0b8c4
BlinkoGamehouse <magicblinkoreviews@gmail.com>
2024-12-20 16:49:40 -0500

    Character Loader script fixed

--- the-candle-project/MAIN/character_loader.gd
+++ the-candle-project/MAIN/character_loader.gd
@@ -3,16 +3,15 @@ extends Node
 signal load_character
 
 @export var character: CharacterBody2D
-@export var old_sprite: Sprite2D
 
 func _on_load_character() -> void:
 	var newnode = load("res://MAIN/CharacterSaves/CharacterSave1.tscn").instantiate()
 	
 	#scale new sprite to size of old sprite*scale of character 
-	newnode.scale = old_sprite.texture.get_size()/(newnode.get_child(1).texture.get_size()*character.scale)
+	newnode.scale = character.texture.get_size()/(newnode.get_child(1).texture.get_size()*character.scale)
 	
 	#delete old sprite and add new one
-	old_sprite.queue_free()
+	character.texture=null
 	character.add_child(newnode)
 	
 	queue_free() #delete character loader. His job is done!
